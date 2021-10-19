1c58686a3d693b727779fd6b756937c22a84c313
Barend Erasmus <hirebarend@gmail.com>
2021-10-19 13:27:43 +0200

    update

--- transactions/index.ts
+++ transactions/index.ts
@@ -8,18 +8,35 @@ const httpTrigger: AzureFunction = async function (
   const container = await Container.get();
 
   try {
-    const result = await container.transactionService.create(
-      req.params.reference,
-      req.body.amount,
-      req.body.collectionReference,
-      req.body.metadata,
-      req.body.reference,
-      req.body.type
-    );
+    if (req.body instanceof Array) {
+      const result =
+        await container.transactionService.createProcessCompleteMultiple(
+          req.body
+        );
 
-    context.res = {
-      body: result.transaction,
-    };
+      context.res = {
+        body: result,
+      };
+
+      return;
+    }
+
+    if (req.body instanceof Object) {
+      const result = await container.transactionService.createProcessComplete(
+        req.params.reference,
+        req.body.amount,
+        req.body.collectionReference,
+        req.body.metadata,
+        req.body.reference,
+        req.body.type
+      );
+
+      context.res = {
+        body: result.transaction,
+      };
+
+      return;
+    }
   } catch (error) {
     context.res = {
       status: 400,
