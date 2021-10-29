c9b692bda3235faeaa894612e477647ae3898178
Barend Erasmus <hirebarend@gmail.com>
2021-10-29 15:47:38 +0200

    add transactions route

---
+++ account-transactions/index.ts
@@ -0,0 +1,66 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { ITransaction } from 'sqrtledger-core';
+import { Container } from '../core';
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    const container = await Container.get();
+
+    if (req.method === 'GET') {
+      const transactions: Array<ITransaction> =
+        await container.transactionService.findAll(req.params.reference, {});
+
+      context.res = {
+        body: transactions,
+      };
+
+      return;
+    }
+
+    if (req.method === 'POST') {
+      if (req.body instanceof Array) {
+        const result =
+          await container.transactionService.createProcessCompleteMultiple(
+            req.body
+          );
+
+        context.res = {
+          body: result,
+        };
+
+        return;
+      }
+
+      if (req.body instanceof Object) {
+        const result = await container.transactionService.createProcessComplete(
+          req.params.reference,
+          req.body.amount,
+          req.body.collectionReference,
+          req.body.metadata,
+          req.body.reference,
+          req.body.type
+        );
+
+        context.res = {
+          body: result.transaction,
+        };
+
+        return;
+      }
+    }
+  } catch (error) {
+    context.log(`[ERROR]: ${error.message}`);
+
+    context.res = {
+      body: {
+        message: error.message,
+      },
+      status: 500,
+    };
+  }
+};
+
+export default httpTrigger;
